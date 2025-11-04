cf2702b39c0a1401639a0f0baed8fc6a592436ec
MounikaD13 <mounika.d3945@gmail.com>
2025-11-04 12:17:46 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import CategoryFilter from './components/CategoryFilter.jsx'
+import MovieList from './components/MovieList'
+import SearchBar from './components/SearchBar'
 
 export default function App() {
   return (
     <div>
-      
+      <MovieList/>
     </div>
   )
 }
