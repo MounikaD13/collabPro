c5a6b9fc8d704c066b9558f30b3fa74a09848122
MounikaD13 <mounika.d3945@gmail.com>
2025-11-04 12:48:42 +0530

    fi

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
-export default function CategoryFilter() {
+export default function CategoryFilter({ categories, selectedCategory, setSelectedCategory }) {
   return (
-    <div>
-      
+    <div style={{marginLeft:"30px"}}>
+      <select 
+        style={{
+          borderRadius: "10px",
+          width: "30%",
+          padding: "10px",
+          fontSize: "1.2rem"
+        }}
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}>
+        <option value="">All Categories </option>
+        {
+          categories.map((i) => (
+            <option key={i} value={i}>
+              {i}
+            </option>
+          ))
+        }
+      </select>
     </div>
   )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import SearchBar from './SearchBar'
+import CategoryFilter from './CategoryFilter'
+import moviesData from '../data.json'
 
 export default function MovieList() {
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  const [selectedCategory, setSelectedCategory] = useState("")
+  const [filteredMovies, setFilteredMovies] = useState([])
+  useEffect(() => {
+    setMovies(moviesData)
+    setFilteredMovies(moviesData)
+  }, [])
+  useEffect(() => {
+    const newFiltered = movies.filter((i) => {
+      const matchMovieName = i.name.toLowerCase().includes(searchTerm.toLowerCase())
+      const matchSelectedCategory = selectedCategory ? i.category === selectedCategory : true
+      return matchMovieName && matchSelectedCategory
+    })
+    setFilteredMovies(newFiltered)
+  }, [selectedCategory, searchTerm, movies])
+  const categories = [...new Set(movies.map((m) => m.category))]
   return (
     <div>
-      
+      <h1 className='text-center'>Movies Search App</h1>
+      <SearchBar Search={searchTerm} setSearchTerm={setSearchTerm}/>
+      {/* {console.log(movies)} */}
+      <CategoryFilter
+        categories={categories}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}/>
+      {
+        filteredMovies.length == 0 ? (
+          <div>Movie not found</div>
+        ) : (
+          <div className="container mt-4">
+            <div className="row g-3">
+              {filteredMovies.map((i) => (
+                <div key={i.id} className="col-md-4">
+                  <div className="card h-100 shadow-sm">
+                    <div className="card-body">
+                      <h5 className="card-title">{i.name}</h5>
+                      <p className="card-text"><strong>Year:</strong> {i.year}</p>
+                      <p className="card-text"><strong>Category:</strong> {i.category}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+        )
+      }
     </div>
   )
 }
